58bad9d69799170ed5ef08f1bdaeee118d4b7183
Tianming Xu <txu1@haverford.edu>
2018-04-01 13:15:36 -0400

    A simple break the bricks
    Try to be more familiar with how to use Unity

---
+++ BreakTheBricks/Assets/Script/Shoot.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour {
+    public GameObject bullet;
+    public float speed = 20;
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Hello Unity");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0)) {
+            //getmousebuttondown(0) is for detecting the press of left button
+            GameObject b1 = GameObject.Instantiate(bullet, transform.position, transform.rotation);
+            Rigidbody rgb = b1.GetComponent<Rigidbody>(); //here we use function to get the rigid body component of object
+            rgb.velocity = transform.forward * speed; //here the transform means the object we attach our script to, forward depends on its direction
+        }
+	}
+}
---
+++ BreakTheBricks/Assets/Script/move_cam.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move_cam : MonoBehaviour {
+    public float speed = 3;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float hori = Input.GetAxis("Horizontal");
+        float verti = Input.GetAxis("Vertical");
+        transform.Translate(new Vector3(hori, verti, 0) * Time.deltaTime * speed); // we change the attached object by detecting we press left or right key(change will go to -1 or 1)
+        //new Vector3 I guess represents for 3 axis.
+        //only change x-axis now
+        //Time.deltaTime means we let object move change/sec
+	}
+}
